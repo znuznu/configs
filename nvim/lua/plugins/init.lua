483099c2c48602ae39a560f342e5dc5d41116a54
znuznu <znu_1@protonmail.com>
2026-02-21 14:37:02 +0100

    add opencode plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -57,4 +57,39 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
   },
+
+  {
+    "nickjvandyke/opencode.nvim",
+    version = "*", -- Latest stable release
+    lazy = false,
+    dependencies = {
+      {
+        -- `snacks.nvim` integration is recommended, but optional
+        ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+        "folke/snacks.nvim",
+        optional = true,
+        opts = {
+          input = {}, -- Enhances `ask()`
+          picker = { -- Enhances `select()`
+            actions = {
+              opencode_send = function(...)
+                return require("opencode").snacks_picker_send(...)
+              end,
+            },
+            win = {
+              input = {
+                keys = {
+                  ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+                },
+              },
+            },
+          },
+          terminal = {}, -- Enables the `snacks` provider
+        },
+      },
+    },
+    config = function()
+      require "configs.opencode"
+    end,
+  },
 }
