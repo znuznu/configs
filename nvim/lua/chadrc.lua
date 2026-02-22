8f70635e0a1804004875188c45c4af90e3fdadf5
znuznu <znu_1@protonmail.com>
2026-02-22 09:26:12 +0100

    ui improvements

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,6 +1,5 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
@@ -18,6 +17,11 @@ M.ui = {
   statusline = {
     theme = "minimal",
   },
+  telescope = { style = "bordered" },
+}
+
+M.nvdash = {
+  load_on_startup = true,
 }
 
 return M
